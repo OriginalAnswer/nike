89eb66cf818f991a51ccf59a4af1ffb98bc92efd
OriginalAnswer <46561454+OriginalAnswer@users.noreply.github.com>
2019-02-08 11:25:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Nike Info</title>
-    <link rel="stylesheet" href="http://localhost/Coding_school/nike/css/style.css">
+    <link rel="stylesheet" href="http://localhost/Coding_school/nike/style.css">
   </head>
   <body>
     <header>
